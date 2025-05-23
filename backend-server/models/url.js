6fcd7ad9c580c804daf95a188fd1b890db1f0f60
patnana praveen <patnanapraveen22@gmail.com>
2025-05-24 01:08:18 +0530

    add url model

---
+++ backend-server/models/url.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const urlSchema = new mongoose.Schema({
+  shortId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  longUrl: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Url', urlSchema);
+
