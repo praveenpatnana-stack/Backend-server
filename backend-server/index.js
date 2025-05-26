a7765d152e52f983060921bf7912323a6b087c5a
patnana praveen <patnanapraveen22@gmail.com>
2025-05-26 22:26:30 +0530

    Update index.js

--- backend-server/index.js
+++ backend-server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { nanoid } = require('nanoid');
 const cors = require('cors');
-const Url = require('./models/Url');
+const Url = require('./models/url');
 
 const app = express();
 const PORT = process.env.PORT || 10000;
